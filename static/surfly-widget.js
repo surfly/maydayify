1c874f67abc9a5369cf8a54cc96f2a43e832fcee
Nicholas Piël <nicholas@surfly.com>
2014-06-26 15:06:30 +0200

    widget now can integrated by others

--- static/surfly-widget.js
+++ static/surfly-widget.js
@@ -1,17 +1,18 @@
 (function() {
 
-    //var CONTINUATION_POINT = "http://pinacot.com/con.php";
-    //var PROXY_POINT = "http://purged.info/start/?url="
-
     var iframe;
 
     window.surfly_session_start = function () {
-        //var widget = document.getElementById('surflywidget');
-        //widget.innerHTML = "Please wait...<img src='http://pinacot.com/ajax-loader.gif' style='float:right'></img>"
-        _get_continuation_url(window.location.href, function(continuation_url) {
-            iframe = _create_iframe();
-            iframe.src = PROXY_POINT+encodeURIComponent(CONTINUATION_POINT+continuation_url);
-        });
+        if (window.CONTINUATION_POINT) {
+            _get_continuation_url(window.location.href, function(continuation_url) {
+                iframe = _create_iframe();
+                iframe.src = PROXY_POINT+encodeURIComponent(CONTINUATION_POINT+continuation_url);
+            });
+        } else {
+                iframe = _create_iframe();
+                iframe.src = PROXY_POINT+window.location.href;
+        }
+
     };
 
     function show_iframe() {
@@ -37,20 +38,11 @@
         };
     }
 
-    function show_widget() {
+    function show_frame() {
         window.addEventListener("message", receiveMessage, false);
         if (window.__surfly) {
-            window.setTimeout( function() {
-                window.__surfly_CF.parent.parent.postMessage(JSON.stringify({'surfly':{ type:'widget'}}), "*");
-            }, 3000);
-            return;
+            window.__surfly_CF.parent.parent.postMessage(JSON.stringify({'surfly':{ type:'widget'}}), "*");
         }
-        var div = document.createElement('div');
-        div.innerHTML = "<a id='continuator' href='' style='color: white; text-decoration: none'>start session</a>";
-        div.id = "surflywidget";
-        div.style.cssText = "padding: 8px; text-align: center; color: white; background: red; position: fixed; bottom: 0px; right: 0px; width: 150px; height: 30px";
-        document.body.appendChild(div);
-        document.getElementById('continuator').onclick = function() { session_start(); return false;};
     };
 
 
@@ -82,7 +74,7 @@
         iframe.setAttribute('frameborder', '0')
         iframe.setAttribute('hspace', '0')
         iframe.setAttribute('vspace', '0')
-        iframe.style.cssText = "position: fixed; top: 0px; left: 0px; background: pink; display: none; overflow: auto; z-index: 6000";
+        iframe.style.cssText = "position: fixed; top: 0px; left: 0px; display: none; overflow: auto; z-index: 6000";
         document.body.appendChild(iframe);
         return iframe;
     };
@@ -94,9 +86,9 @@
     function _synchronize_iframe() {
         var source_list = _get_form_elements();
         var obj = { 'surfly' : { 'type': 'formfill', 'sourcelist': source_list }};
-        iframe.contentWindow.frames[0].frames[0].postMessage(JSON.stringify(obj), "*");
+        iframe.contentWindow.frames[0].postMessage(JSON.stringify(obj), "*");
         obj = { 'surfly' : { type: 'scroll', 'ypos': window.pageYOffset, 'xpos': window.pageXoffset }};
-        iframe.contentWindow.frames[0].frames[0].postMessage(JSON.stringify(obj), "*");
+        iframe.contentWindow.frames[0].postMessage(JSON.stringify(obj), "*");
     };
 
     function _get_form_elements() {
@@ -106,10 +98,7 @@
             var children = forms[i].getElementsByTagName('*');
             for (var j=0, m = children.length; j<m; j++) {
                 var child = children[j];
-                if ((child.tagName == "OPTION") ||
-                   ((child.tagName == "INPUT") && child.type == "radio") ||
-                   ((child.tagName == "INPUT") && child.type == "checkbox") ||
-                   ((child.tagName == "INPUT") && child.type == "text")) {
+                if ((child.tagName == "OPTION") || (child.tagName == "INPUT")) {
                     target_list.push([ child.checked, child.selected, child.disabled, child.value])
                 }
             }
@@ -127,21 +116,19 @@
             var children = forms[i].getElementsByTagName('*');
             for (var j=0, m = children.length; j<m; j++) {
                 var child = children[j];
-                if ((child.tagName == "OPTION") ||
-                   ((child.tagName == "INPUT") && child.type == "radio") ||
-                   ((child.tagName == "INPUT") && child.type == "checkbox") ||
-                   ((child.tagName == "INPUT") && child.type == "text")) {
+                if ((child.tagName == "OPTION") || (child.tagName == "INPUT")) {
                     source = sourcelist[c];
                     child.checked = source[0];
                     child.selected = source[1];
                     child.disabled = source[2];
                     child.value = source[3];
+                    c++;
                 }
             }
         }
         return target_list;
     }
 
-    //window.addEventListener('load', show_widget);
+    window.addEventListener('load', show_frame);
 
 })();
